3bdf5f66df7cdfbe30d9bf6b2a29d104e89d2ff6
tingyu <1608386461@qq.com>
2024-09-19 16:40:12 +0800

    主页个人信息配置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/wnblog/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "汪宁，你好",
+  description: "我的第一个VuePress站点",
 
   theme,
 
